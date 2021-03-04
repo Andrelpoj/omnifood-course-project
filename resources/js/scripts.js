3ed53250eed5ee449808ac0e04789834ac1923f2
Andre Pereira <andrelpoj@gmail.com>
2021-03-04 09:20:02 -0300

    Feat - Animations

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -21,8 +21,7 @@ $(document).ready(function() {
     })
 
     /* Navigation Scroll */
-    
-        /* From: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+    /* From: https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
         $('a[href*="#"]')
             .not('[href="#"]')
             .not('[href="#0"]')
@@ -49,4 +48,29 @@ $(document).ready(function() {
                     }
                 }
         })
+
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn')
+    }, {
+        offset: '50%'
+    })
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse')
+    }, {
+        offset: '50%'
+    })
 })
